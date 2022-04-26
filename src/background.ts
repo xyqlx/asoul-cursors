4036f92680b3a86dd833757693d00f6d6996bc73
xyqlx <saint.diana@outlook.com>
2022-04-26 15:43:56 +0800

    fixed 失效bug #1 reason: code in onInstalled only run once

--- src/background.ts
+++ src/background.ts
@@ -6,33 +6,33 @@ chrome.runtime.onInstalled.addListener(() => {
         title: '光标设置',
         contexts: ['page', 'action']
     });
-    chrome.contextMenus.onClicked.addListener(
-        (info, tab) => {
-            if (info.menuItemId === 'setting') {
-                chrome.windows.create({
-                    url: chrome.runtime.getURL("index.html"),
-                    type: "popup",
-                    width: 300,
-                    height: 500
-                }, function (win) {
-                    // win represents the Window object from windows API
-                    // Do something after opening
-                });
-            }
-        }
-    );
-    chrome.webNavigation.onCompleted.addListener(() => {
-        const queryOptions = { currentWindow: true };
-        chrome.tabs.query(queryOptions, (tabs) => {
-            tabs.forEach((tab) => {
-                if (tab?.id) {
-                    changeCursor(tab.id);
-                }
+});
+chrome.contextMenus.onClicked.addListener(
+    (info, tab) => {
+        if (info.menuItemId === 'setting') {
+            chrome.windows.create({
+                url: chrome.runtime.getURL("index.html"),
+                type: "popup",
+                width: 300,
+                height: 500
+            }, function (win) {
+                // win represents the Window object from windows API
+                // Do something after opening
             });
+        }
+    }
+);
+chrome.webNavigation.onCompleted.addListener(() => {
+    const queryOptions = { };
+    chrome.tabs.query(queryOptions, (tabs) => {
+        tabs.forEach((tab) => {
+            if (tab?.id) {
+                changeCursor(tab.id);
+            }
         });
-    }, {
-        url: [
-            { urlMatches: `(http|https).*` },
-        ]
     });
+}, {
+    url: [
+        { urlMatches: `(http|https).*` },
+    ]
 });
